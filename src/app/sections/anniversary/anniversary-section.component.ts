b0fa2e241caf53aa1248c6a94f52297e819428f9
kingsolo1975 <144692368+kingsolo1975@users.noreply.github.com>
2026-05-13 01:05:11 +0300

    Use final 25th anniversary coin showcase

--- src/app/sections/anniversary/anniversary-section.component.ts
+++ src/app/sections/anniversary/anniversary-section.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-anniversary-section',
   standalone: true,
-  templateUrl: './anniversary-section.component.html'
+  templateUrl: './anniversary-section.component.html',
+  styleUrl: './anniversary-section.component.css'
 })
 export class AnniversarySectionComponent {}
